b03cdeda238bc135387feb27122a424dc22e96aa
Nikolay Topkaridi <enemycnt@gmail.com>
2022-09-01 15:19:08 +0300

    cleanup fixes

--- packages/example/src/layouts/AppHeader/AppHeader.tsx
+++ packages/example/src/layouts/AppHeader/AppHeader.tsx
@@ -3,7 +3,6 @@ import clsx from "clsx";
 import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
 import Toolbar from "@mui/material/Toolbar";
-// import { Typography } from "@chainsafe/common-components";
 import Typography from "@mui/material/Typography";
 import { Switch, NavLink, Link } from "react-router-dom";
 
--- packages/example/src/layouts/AppHeader/styles.ts
+++ packages/example/src/layouts/AppHeader/styles.ts
@@ -4,27 +4,12 @@ export const useStyles = makeStyles(
   ({ constants, palette, zIndex, breakpoints }: ITheme) => {
     return createStyles({
       root: {
-        // display: "flex",
-        // // position: "fixed",
-        // justifyContent: "space-between",
-        // padding: `${constants.generalUnit * 2}px ${
-        //   constants.generalUnit * 4
-        // }px`,
-        // width: "100%",
-        // top: 0,
-        // left: 0,
         backgroundColor: palette.additional["header"][1],
         borderBottom: `1px solid rgba(255, 122, 69, 0.16)`,
         color: palette.additional["header"][2],
         boxShadow:
           "0px 1px 4px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.02), 0px 1px 12px rgba(0, 0, 0, 0.12)",
         borderRadius: "0px 0px 10.6667px 10.6667px",
-        // fontFamily: 'NeueMontreal, sans-serif'
-        // alignItems: "center",
-        // zIndex: zIndex?.layer2,
-        // [breakpoints.down("sm")]: {
-        //   flexDirection: "column",
-        // },
       },
       left: {
         flexGrow: 1,
@@ -55,8 +40,6 @@ export const useStyles = makeStyles(
         display: "block",
         height: 16,
         width: 16,
-        // borderRadius: "50%",
-        // backgroundColor: palette.additional["green"][6],
         marginRight: constants.generalUnit,
       },
       address: {
@@ -73,7 +56,6 @@ export const useStyles = makeStyles(
       },
       mainInfo: {
         display: "flex",
-        // flexDirection: "column",
       },
       mainTitle: {
         display: "flex",
