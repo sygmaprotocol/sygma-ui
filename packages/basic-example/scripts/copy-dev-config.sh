2e1123846182231df67349acf9794ed7bb5de12d
Nikolay Topkaridi <enemycnt@gmail.com>
2022-09-14 18:59:35 +0300

    removed deprecated basic example

--- packages/basic-example/scripts/copy-dev-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-FILE="./public/sygma-runtime-config.js"
-if [ ! -e $FILE ]; then
-  cp -a ./config/sygma-runtime-config.evm.js ./public/sygma-runtime-config.js
-  echo "Created runtime config for dev in ../public"
-fi
