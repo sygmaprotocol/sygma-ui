22caf50545259d3c63620ea2ec4ed08bd0f4aa8f
Nikolay Topkaridi <enemycnt@users.noreply.github.com>
2022-12-08 14:04:42 +0300

    config server with splitted config (#54)

--- packages/config-server/src/index.ts
+++ packages/config-server/src/index.ts
@@ -5,8 +5,9 @@ import { secrets } from "docker-secret";
 import { SSM } from "@aws-sdk/client-ssm";
 
 const PORT = process.env.PORT || 8000;
-const HOST = process.env.HOST || 'localhost';
-const SSM_PARAMETER_NAME = secrets.SSM_PARAMETER_NAME || process.env.SSM_PARAMETER_NAME
+const HOST = process.env.HOST || "localhost";
+const SSM_PARAMETER_NAME =
+  secrets.SSM_PARAMETER_NAME || process.env.SSM_PARAMETER_NAME;
 
 const app: Application = express();
 app.use(cors());
@@ -24,12 +25,25 @@ const getConfigFromSSM = async () => {
     const rawResponse = data.Parameter?.Value;
     if (rawResponse) {
       const parsedResponse = JSON.parse(rawResponse);
+      if (parsedResponse && parsedResponse.SYGMA.chains.length > 0) {
+        const chains = await Promise.all(
+          parsedResponse.SYGMA.chains.map(async (chainParam: string) => {
+            const rawChainData = await ssm.getParameter({
+              Name: chainParam,
+              WithDecryption: true,
+            });
+            const chainData = JSON.parse(rawChainData.Parameter?.Value!);
+            return chainData;
+          })
+        );
+        parsedResponse.SYGMA.chains = chains;
+      }
       return parsedResponse;
     }
   } catch (e) {
     console.warn("AWS SSM request failed");
     console.error(e);
-    return {error: e}
+    return { error: e };
   }
 };
 
@@ -39,9 +53,9 @@ app.get("/config", (req, res) => {
   });
 });
 
-app.get("/health",  (req, res) => {
-  res.sendStatus(200)
-})
+app.get("/health", (req, res) => {
+  res.sendStatus(200);
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://${HOST}:${PORT}`);
