3d58a0a2ae3de845b840ba77e0603af9155878f9
Anton Lykhoyda <lykhoyda@gmail.com>
2023-02-21 17:12:32 +0100

    added github config on pull request; fixed test runner for sygma-ui

--- packages/sygma-ui/jest.config.js
+++ packages/sygma-ui/jest.config.js
@@ -1,4 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
   // The root of your source code, typically /src
   // `<rootDir>` is a token Jest substitutes
   roots: ["<rootDir>/src"],
